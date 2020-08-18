6d4864ab93eae21cc856005c619707c60858700a
inoubli <aymen.inoubli@gmail.com>
2020-08-18 14:07:52 +0200

    shared greeting.element from library

--- libs/shared-lib/src/lib/greeting.element.ts
+++ libs/shared-lib/src/lib/greeting.element.ts
@@ -2,7 +2,7 @@ export class GreetingElement extends HTMLElement {
     public static observedAttributes = ['title'];
   
     attributeChangedCallback() {
-      this.innerHTML = `<h1>Welcome to ${this.title}!</h1>`;
+      this.innerHTML = `<h1>Welcome to ${this.title}! toto</h1>`;
     }
   }
   
